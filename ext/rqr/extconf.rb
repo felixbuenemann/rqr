6882fc059f1acd9e20f6cf6132cbd713e20be79d
Scott Willson <scott.willson@gmail.com>
2011-11-30 16:08:57 -0800

    Try to remove manual hacks to compile

--- ext/rqr/extconf.rb
+++ ext/rqr/extconf.rb
@@ -11,19 +11,19 @@
 =end
 require 'mkmf'
 require 'rbconfig'
-$libs = append_library($libs, "supc++")
 
 if File.exists?('/sw')
   DARWIN_PORT_DIR = '/sw'
-elsif File.exists?('/opt/local')
-  DARWIN_PORT_DIR = '/opt/local'
 elsif File.exists?('/usr/local')
   DARWIN_PORT_DIR = '/usr/local'
+elsif File.exists?('/opt/local')
+  DARWIN_PORT_DIR = '/opt/local'
 elsif RUBY_PLATFORM =~ /darwin/
   raise "Could not find /sw, /opt/local, or /usr/local. Do you have native library dependencies installed?"
 end
 
 if RUBY_PLATFORM =~ /darwin/
+  dir_config "/Developer/SDKs/MacOSX10.6.sdk/usr/lib"
   dir_config('jpeg', DARWIN_PORT_DIR)
   dir_config('png', DARWIN_PORT_DIR)
   dir_config('tiff', DARWIN_PORT_DIR)
