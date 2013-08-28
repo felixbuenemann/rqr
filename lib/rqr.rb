a29fc9c92e872cc6c7c45542a232fd25eb918c48
Felix Bünemann <buenemann@louis.info>
2013-08-28 13:35:46 +0200

    Fix ruby 2.0 dl deprecation warning (use fiddle)

--- lib/rqr.rb
+++ lib/rqr.rb
@@ -2,7 +2,11 @@ $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
 module RQR
-  require 'dl'
+  begin
+    require 'fiddle'
+  rescue LoadError
+    require 'dl'
+  end
   require "rqr/QR"
   require "rqr/errors"
   require "rqr/qrcode"  
